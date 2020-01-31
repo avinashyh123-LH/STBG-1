da9a821e8b91fca8240df3692627056aa6e9f387
avinashyh123-LH <60501096+avinashyh123-LH@users.noreply.github.com>
2020-01-31 16:11:52 +0530

    Add files via upload

---
+++ STBG_1.java
@@ -0,0 +1,12 @@
+package MavenDemo;
+
+public class STBG_1 {
+
+	public static void main(String[] args) {
+		System.out.println("SSTE");
+		System.out.println("TFLPPA");
+
+		System.out.println("OFSH");
+	}
+
+}
